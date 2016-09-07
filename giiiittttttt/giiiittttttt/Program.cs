1d8bdcf68c89e9e2dd581aa4f7c6002930212fc5
emperorsamie <samieshaya@gmail.com>
2016-09-07 11:07:15 +0200

    Update Program.cs

--- giiiittttttt/giiiittttttt/Program.cs
+++ giiiittttttt/giiiittttttt/Program.cs
@@ -11,6 +11,7 @@ namespace giiiittttttt
         static void Main(string[] args)
         {
             Console.WriteLine("");
+            Console.Write("Vilken vacker kod!");
         }
     }
 }
